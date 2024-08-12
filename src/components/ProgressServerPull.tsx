da462f98a1390e4ade166d8f2f5db29df44bcffa
Mickael Royer <mickael.royer@enac.fr>
2024-08-12 17:57:04 +0200

    Pull docker images : Display error msg when pull fails

--- src/components/ProgressServerPull.tsx
+++ src/components/ProgressServerPull.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useReducer } from 'react';
+import { useTranslation } from 'react-i18next';
 import { PullSrvMsg } from '../proto/netem_pb';
 
-type ServerPullStateT = { [key: string]: PullSrvMsg.Code };
+type ServerPullStateT = { [key: string]: {code: PullSrvMsg.Code; error: string; }};
 const INITIAL_STATE: ServerPullStateT = {}
 
 function reducer(state: ServerPullStateT, msg:PullSrvMsg.AsObject): ServerPullStateT {
     return {
         ...state,
-        [msg.image]: msg.code
+        [msg.image]: {
+            code: msg.code,
+            error: msg.error
+        }
     }
 }
 
 export default function ProgressServerPull(): JSX.Element {
+    const {t} = useTranslation();
     const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
     useEffect(() => {
@@ -25,16 +30,30 @@ export default function ProgressServerPull(): JSX.Element {
             <div className="alert flex flex-col-reverse gap-1">
                 {Object.keys(state).map((image) => (
                     <div className='p-2 w-full'>
-                        <span className='me-2'>Pull image <span className='italic'>{image}</span></span>
+                        <div>
+                            <span className='me-2'>
+                                {t("PullDockerImgProgress", {image, interpolation: {escapeValue: false}})}
+                            </span>
+                            {
+                                state[image].code == PullSrvMsg.Code.START && (
+                                    <progress className="progress progress-info w-32">
+                                    </progress>
+                                )
+                            }
+                            {
+                                state[image].code == PullSrvMsg.Code.OK && (
+                                    <span className='text-green-500 font-semibold flex-1 text-ellipsis'>OK</span>
+                                )
+                            }
+                            {
+                                state[image].code == PullSrvMsg.Code.ERROR && (
+                                    <span className='text-red-500 font-bold'>NOK</span>
+                                )
+                            }
+                        </div>
                         {
-                            state[image] == PullSrvMsg.Code.START && (
-                                <progress className="progress progress-info w-48">
-                                </progress>
-                            )
-                        }
-                        {
-                            state[image] == PullSrvMsg.Code.OK && (
-                                <span className='text-green-500 font-semibold'>OK</span>
+                            state[image].code == PullSrvMsg.Code.ERROR && (
+                                <p className='text-red-500'>{state[image].error}</p>
                             )
                         }
                     </div>
